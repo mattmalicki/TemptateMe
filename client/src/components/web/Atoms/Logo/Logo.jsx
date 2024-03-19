1c75cfc844583f19e20328144859bd1a1e18418b
mattmalicki <mateusz.malicki96@gmail.com>
2024-03-20 00:12:37 +0100

    Fixing smth in Logo.

--- client/src/components/web/Atoms/Logo/Logo.jsx
+++ client/src/components/web/Atoms/Logo/Logo.jsx
@@ -1,11 +1,7 @@
 import { ReactComponent as LogoIcon } from "./icon-logo.svg";
 
-const Logo = (size) => {
-  return (
-    <div style={`width: ${size}px, height: ${size}px`}>
-      <LogoIcon />
-    </div>
-  );
+const Logo = () => {
+  return <LogoIcon />;
 };
 
 export { Logo };
