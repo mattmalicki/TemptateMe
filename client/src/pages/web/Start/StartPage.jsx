cd60e45af1e3215836b2255a3cdcb22696e87831
mattmalicki <mateusz.malicki96@gmail.com>
2024-03-20 14:34:31 +0100

    Adding StartPage.

---
+++ client/src/pages/web/Start/StartPage.jsx
@@ -0,0 +1,29 @@
+import styles from "./StartPage.module.css";
+import { Logo } from "../../../components/web/Atoms/Logo/Logo.jsx";
+import { CurvedButton } from "../../../components/web/Atoms/CurvedButton/CurvedButton.jsx";
+
+const StartPage = () => {
+  return (
+    <section className={styles.StartPage}>
+      <div className={styles.icon}>
+        <Logo />
+      </div>
+      <h1 className={styles.title}>Welcome to the app!</h1>
+      <p className={styles.comment}>
+        This app offers more than just a collection of recipes - it is designed
+        to be your very own digital cookbook. You can easily save and retrieve
+        your own recipes at any time.
+      </p>
+      <div className={styles.buttons}>
+        <a href="google.com">
+          <CurvedButton title="Registration" greenOrBlack="green" />
+        </a>
+        <a href="google.com">
+          <CurvedButton title="Sign in" />
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export { StartPage };
