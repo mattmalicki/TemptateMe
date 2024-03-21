5625e07798938363528943ea21156d82193e57aa
mattmalicki <mateusz.malicki96@gmail.com>
2024-03-21 15:15:12 +0100

    AuthPage styling completed.

--- client/src/pages/web/Auth/AuthPage.jsx
+++ client/src/pages/web/Auth/AuthPage.jsx
@@ -1,9 +1,11 @@
 import styles from "./AuthPage.module.css";
 import { AuthForm } from "../../../components/web/Organisms/AuthForm/AuthForm.jsx";
+import { AuthPageImage } from "../../../components/web/Atoms/AuthPageImage/AuthPageImage.jsx";
 
 const AuthPage = ({ isRegister }) => {
   return (
     <div className={styles.AuthPage}>
+      <AuthPageImage />
       <AuthForm isRegister={isRegister} />
     </div>
   );
