217e4c59e6916fcfe960436fb828eccf281d7223
mattmalicki <mateusz.malicki96@gmail.com>
2024-03-24 17:19:38 +0100

    Adding Footer component.

---
+++ client/src/components/Organisms/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import styles from "./Footer.module.css";
+import { Navigation } from "../Navigation/Navigation.jsx";
+import { Socials } from "../../Molecules/Socials/Socials.jsx";
+import { FooterTerms } from "../../Atoms/FooterTerms/FooterTerms.jsx";
+import { Newsletter } from "../../Molecules/Newsletter/Newsletter.jsx";
+import { FooterInfo } from "../../Molecules/FooterInfo/FooterInfo.jsx";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <footer className={styles.Footer}>
+        <FooterInfo />
+        <Navigation isFooter={true} />
+        <Newsletter />
+        <Socials />
+      </footer>
+      <FooterTerms />
+    </div>
+  );
+};
+
+export { Footer };
