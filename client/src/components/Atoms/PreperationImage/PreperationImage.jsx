a819816c021b6afc066b2c01798d13cc49a93489
mattmalicki <mateusz.malicki96@gmail.com>
2024-04-25 00:12:09 +0200

    misspell.

--- client/src/components/Atoms/PreperationImage/PreperationImage.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./PreperationImage.module.css";
-
-const PreperationImage = ({ src, alt }) => {
-  return <img alt={alt} src={src} className={styles.PreperationImage} />;
-};
-
-export { PreperationImage };
