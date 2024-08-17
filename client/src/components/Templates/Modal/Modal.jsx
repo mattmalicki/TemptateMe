ceab8c47ef86aa22a6f4dd6727c17b296d7b2ac0
mattmalicki <mateusz.malicki96@gmail.com>
2024-08-17 04:47:07 +0200

    Created modal for loggin out.

--- client/src/components/Templates/Modal/Modal.jsx
+++ client/src/components/Templates/Modal/Modal.jsx
@@ -3,9 +3,9 @@ import { ReactComponent as IconClose } from "./icon-close.svg";
 
 const Modal = ({ onClose = () => {}, children }) => {
   return (
-    <div className={styles.backdrop}>
-      <div className={styles.Modal}>
-        <button type="button" onClick={onClose}>
+    <div className={styles.backdrop} id="backdrop">
+      <div className={styles.Modal} data-modal="true">
+        <button className={styles.button} type="button" onClick={onClose}>
           <IconClose />
         </button>
         {children}
