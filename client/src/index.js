ef69d3d3f626d4a1b39d539fc6e82b844499c454
mattmalicki <mateusz.malicki96@gmail.com>
2024-08-20 18:17:12 +0200

    Wrapping App in DarkModeProvider.

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,8 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 
+import { DarkModeProvider } from "./context/DarkModeContext/DarkModeContext.js";
+
 import { store, persistor } from "./redux/store";
 
 import "./index.css";
@@ -15,7 +17,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <DarkModeProvider>
+            <App />
+          </DarkModeProvider>
         </BrowserRouter>
       </PersistGate>
     </Provider>
